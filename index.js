b0368f33b4b32fbb0bfdd4e985d282d0ec0d841a
codeLearn23 <kj.koff@gmail.com>
2026-04-21 13:58:34 -0400

    Refactor JS to use textContent rather than innerHTML and improve variable naming

--- index.js
+++ index.js
@@ -42,50 +42,88 @@ const posts = [
 // grab the <main> element that will hold all the child elements we're about to create
 const root = document.getElementById("root");
 
-posts.forEach(post => {
+posts.forEach(item => {
     // for each object, we will create an HMTL element and append content to it.
     // we're calling it mainContainer
     // mainContainer is basically the card that the user will look at on the screen.
     const mainContainer = document.createElement("div");
     mainContainer.id = "main-container";
-    const item = post;
     
+    // avatar section
     const avatar = document.createElement("section");
-    avatar.classList.add("avatar-section");
-    avatar.innerHTML = `
-        <img src="./${item.avatar}" alt="User avatar photo." class="avatar-pic" />
-        <div>
-            <h1 class="avatar-name bold">${item.name}</h1>
-            <p class="avatar-location regular">${item.location}</p>
-        </div>
-        `
+    avatar.className = "avatar-section";
+    const av = document.createElement("img");
+    av.src = `./${item.avatar}`;
+    av.alt = "User avatar photo.";
+    av.className = "avatar-pic";
+    avatar.append(av);
+
+    const div = document.createElement("div");
+    const h1 = document.createElement("h1");
+    h1.className = "avatar-name bold";
+    const p = document.createElement("p");
+    p.className = "avatar-location regular";
+    h1.textContent = item.name;
+    p.textContent = item.location;
+    div.append(h1, p);
+    avatar.append(div);
     mainContainer.append(avatar);
     
+    // picture section (the post that the user is sharing)
     const pic = document.createElement("section");
-    pic.classList.add("pic-section");
-    pic.innerHTML = `
-        <img src="./${item.post}" alt="${item.name} painted self portrait." class="post-image" />`;
+    pic.className = "pic-section";
+    const post = document.createElement("img");
+    post.src = `./${item.post}`;
+    post.alt = `${item.name} painted self portrait.`;
+    post.className = "post-image";
+    pic.append(post);
     mainContainer.append(pic);
-    
+
+    // response section: icons, likes, and comment
     const responseSection = document.createElement("section");
-    responseSection.classList.add("response-section");
-    responseSection.innerHTML = `
-        <div class="icon-bar">
-        <img src="./images/icon-heart.png" alt="Heart icon." />
-        <img src="./images/icon-comment.png" alt="Speech bubble icon." />
-        <img src="./images/icon-dm.png" alt="Paper airplane icon." />
-        </div>
-        <p class="bold">${item.likes} likes</p>
-    `
+    responseSection.className = "response-section";
+
+    const iconBar = document.createElement("div");
+    iconBar.className = "icon-bar";
+    
+    const heart = document.createElement("img");
+    heart.src = "./images/icon-heart.png";
+    heart.alt = "Heart icon.";
+    
+    const comment = document.createElement("img");
+    comment.src = "./images/icon-comment.png";
+    comment.alt = "Speech bubble icon.";
+    
+    const dm = document.createElement("img");
+    dm.src = "./images/icon-dm.png";
+    dm.alt = "Paper airplane icon.";
+
+    iconBar.append(heart, comment, dm);
+    responseSection.append(iconBar);
+
+    const likes = document.createElement("p");
+    likes.className = "bold";
+    likes.textContent = `${item.likes} likes`;
+    responseSection.append(likes);
+
     mainContainer.append(responseSection);
     
+
+    // user post section
     const userpostSection = document.createElement("section");
-    userpostSection.classList.add("user-post-section");
-    userpostSection.innerHTML = `
-        <p class="regular"><span class="bold">${item.username}</span> ${item.comment}</p>
-    `
+    userpostSection.className = "user-post-section";
+    const userWords = document.createElement("p");
+    userWords.className = "regular";
+    
+    const spanned = document.createElement("span");
+    spanned.className = "bold";
+    spanned.textContent = item.username;
+    userWords.append(spanned, ` ${item.comment}`);
+    userpostSection.append(userWords);
+    
     mainContainer.append(userpostSection);
     
-    // now that we've built a mainContainer (a card), add it to <main> (the root element)
+    // append mainContainer to root
     root.append(mainContainer);
+    // end of loop
 });
